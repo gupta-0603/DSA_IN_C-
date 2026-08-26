4b20c74622a6bb9b01134cae3622497fcf28e4f1
Aditya kumar <adityagupta841101@gmail.com>
2026-08-27 00:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.48%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int sumofsquares(int n) {
+    int sum=0;
+    while(n>0){
+        int digit=n%10;
+        sum+=digit*digit;
+        n/=10;
+    }
+    return sum;
+}
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do {
+            slow=sumofsquares(slow);
+            fast=sumofsquares(sumofsquares(fast));
+        } while(slow!=fast); return slow==1;
+    }
+};
